67928b9f5907947c1432efe3f633ca09cdeb912a
Nickolay Trukhin <coolone.official@gmail.com>
2020-01-26 14:23:51 +0300

    Added game logic and ui

--- epam_firstApp/ViewController.swift
+++ epam_firstApp/ViewController.swift
@@ -12,9 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        titleLabel.contentMode = .scaleToFill
+        titleLabel.numberOfLines = 0
+        attemptButton.setTitle(
+            "Attempt \(attemptsCount + 1)",
+            for: .normal
+        )
     }
 
-
+    @IBAction func onAttemptClicked(_ sender: Any) {
+        let inputNumber = Int(numberTextField.text!)!
+        
+        var labelStr: String
+        if inputNumber > randomNumber {
+            labelStr = "Too much"
+            attemptsCount += 1
+        } else if inputNumber < randomNumber {
+            labelStr = "Too small"
+            attemptsCount += 1
+        } else {
+            labelStr = "You win using \(attemptsCount) \(attemptsCount > 1 ? "attempts" : "attempt"). Random number regenerated."
+            randomNumber = UInt8.random(in: 0...99)
+            attemptsCount = 0
+        }
+        
+        titleLabel.text = labelStr
+        attemptButton.setTitle(
+            "Attempt \(attemptsCount + 1)",
+            for: .normal
+        )
+    }
+    
+    var randomNumber: UInt8 = UInt8.random(in: 0...99)
+    var attemptsCount: UInt = 0
+    
+    @IBOutlet var numberTextField: UITextField!
+    @IBOutlet var attemptButton: UIButton!
+    @IBOutlet var titleLabel: UILabel!
 }
 
