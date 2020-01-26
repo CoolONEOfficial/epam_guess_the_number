a5e099d59317a3a25e499d03ff2137a7c73f5268
Nickolay Trukhin <coolone.official@gmail.com>
2020-01-26 13:19:20 +0300

    Initial Commit

---
+++ epam_firstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  epam_firstApp
+//
+//  Created by Nickolay Truhin on 26.01.2020.
+//  Copyright © 2020 Nickolay Truhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ epam_firstAppTests/epam_firstAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  epam_firstAppTests.swift
+//  epam_firstAppTests
+//
+//  Created by Nickolay Truhin on 26.01.2020.
+//  Copyright © 2020 Nickolay Truhin. All rights reserved.
+//
+
+import XCTest
+@testable import epam_firstApp
+
+class epam_firstAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
